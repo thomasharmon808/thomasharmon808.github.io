b5a35d7ac2cecd3807e6d6dab6323cad1c12090d
Tom Harmon <thomas.alan.harmon@gmail.com>
2019-06-27 18:42:16 -0400

    added config file to contain basic site information

---
+++ src/config/config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  siteTitle: 'Thomas Harmon | Software Engineer',
+  siteDescription:
+    'Thomas Harmon is a software engineer in Boston, MA who carries a broad set of skills, ranging from electrical and computer engineering, data science, machine learning, to full stack stoftware development.',
+  siteKeywords:
+    'Thomas Harmon, Thomas, Harmon, tharmon808, thomasharmon808, software engineer, front-end engineer, back-end engineer, full-stack engineer, northeastern, neu, boston',
+  siteUrl: 'https://harmon.tech',
+  siteLanguage: 'en_US',
+
+  name: 'Thomas Harmon',
+  location: 'Boston, MA',
+  email: 'thomas.alan.harmon@gmail.com',
+  github: 'https://github.com/thomasharmon808/',
+  socialMedia: [
+    {
+      name: 'Github',
+      url: 'https://github.com/thomasharmon808/',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/tharmony/',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/tomthegrom808/',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/thomasharmon808',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '#about',
+    },
+    {
+      name: 'Experience',
+      url: '#experience',
+    },
+    {
+      name: 'Projects',
+      url: '#projects',
+    },
+    {
+      name: 'Contact',
+      url: '#contact',
+    },
+  ],
+
+  twitterHandle: '@thomasharmon808',
+  //googleAnalyticsID: '',
+  //googleVerification: '',
+};
