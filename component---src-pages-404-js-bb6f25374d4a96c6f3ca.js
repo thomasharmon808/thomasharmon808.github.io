4d4b1c2cf31e0294ddce04c045d0c707f45b00e0
Tom Harmon <thomas.alan.harmon@gmail.com>
2019-08-04 07:59:02 -0400

    added hero to prod

---
+++ component---src-pages-404-js-bb6f25374d4a96c6f3ca.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{191:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),s=n(196);t.default=function(){return u.a.createElement(s.a,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bb6f25374d4a96c6f3ca.js.map
