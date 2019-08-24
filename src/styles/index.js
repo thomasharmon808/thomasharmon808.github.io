e3c7be63685854e2ca7e7c26c139e202c96df5af
thomas harmon <thomas.alan.harmon@gmail.com>
2019-08-24 14:13:27 -0400

    deleted unused files

--- src/styles/index.js
+++ src/styles/index.js
@@ -2,24 +2,8 @@ import theme from "./theme"
 import media from "./media"
 import GlobalStyle from "./globalstyle"
 
-// import Header from './Header';
-// import Footer from './Footer';
-
-// import Main from './Main';
-// import Section from './Section';
-// import Heading from './Heading';
-// import Button from './Button';
-// import InlineLink from './InlineLink';
-
 export {
   theme,
   media,
-  GlobalStyle,
-  // Header,
-  // Footer,
-  // Main,
-  // Section,
-  // Heading,
-  // Button,
-  // InlineLink,
+  GlobalStyle
 }
